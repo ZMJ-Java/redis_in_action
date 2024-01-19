e644e829937f41aa03dca9f99b6649284ba81df6
ZMJ-Java <1486480852@qq.com>
2024-01-20 04:18:17 +0800

    配置日志，测试注释

--- src/main/java/com/zmj/redis/RedisInActionApplication.java
+++ src/main/java/com/zmj/redis/RedisInActionApplication.java
@@ -2,7 +2,10 @@ package com.zmj.redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * @author  zmj
+ * @date 2024/1/20 4:16
+ **/
 @SpringBootApplication
 public class RedisInActionApplication {
 	public static void main(String[] args) {
