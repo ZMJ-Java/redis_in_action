686cf5b4a99d856e063b1116bb4bfb02e3176744
ZMJ-Java <1486480852@qq.com>
2024-01-23 00:39:27 +0800

    文章分组服务

---
+++ src/main/java/com/zmj/redis/service/ArticleGroupService.java
@@ -0,0 +1,10 @@
+package com.zmj.redis.service;
+
+/**
+ * @author 14864
+ * @apiNote 文章分组service
+ * @date 2024/1/22 22:40
+ */
+public interface ArticleGroupService {
+
+}
---
+++ src/main/java/com/zmj/redis/service/impl/ArticleGroupServiceImpl.java
@@ -0,0 +1,13 @@
+package com.zmj.redis.service.impl;
+
+import com.zmj.redis.service.ArticleGroupService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author 14864
+ * @apiNote 文章分组实现
+ * @date 2024/1/22 22:40
+ */
+@Service
+public class ArticleGroupServiceImpl implements ArticleGroupService {
+}
